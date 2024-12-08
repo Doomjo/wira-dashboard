a7dc824cab9749835ba4ad0be4a2af4435fe6ba4
Cheong Kin Fai <112203456+Doomjo@users.noreply.github.com>
2024-12-09 02:50:48 +0800

    front end vue dashboard

---
+++ frontend/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <PlayerDashboard>
+
+    </PlayerDashboard>
+  </div>
+</template>
+
+<script>
+import PlayerDashboard from './components/PlayerDashboard.vue';
+export default {
+  name: 'App',
+  components: {
+    PlayerDashboard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: bg-white;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
